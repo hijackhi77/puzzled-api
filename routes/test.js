3d20675287fce7d7b1d4e772296fdcb7e3ffc8d9
Chuan Liu <legojackliu@gmail.com>
2019-12-20 02:00:24 -0800

    Implemented test sign api

--- routes/test.js
+++ routes/test.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const { sign } = require('../lib/utils')
 
 router.get('/', (req, res, next) => {
     res.send('test')
+})
+
+router.get('/sign', (req, res, next) => {
+    res.status(200).send(sign(req.body, 'secret'))
+})
+
+router.get('/validate-sign', (req, res, next) => {
+    res.status(200).send({ isValid: sign(req.body, 'secret') == req.body.sign })
 })
 
 module.exports = router
