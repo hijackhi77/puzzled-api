a142ad2f81a197b9dcded019a6c8e36899009a4b
hijackhi77 <legojackliu@gmail.com>
2021-01-03 01:47:56 -0500

    Implement NQueens solver

--- src/routers/api.ts
+++ src/routers/api.ts
@@ -4,6 +4,9 @@ export const router = express.Router();
 import { router as slidingPuzzleRouter } from './slidingPuzzle';
 router.use('/sliding-puzzle', slidingPuzzleRouter);
 
+import { router as nQueensRouter } from './nQueens';
+router.use('/n-queens', nQueensRouter);
+
 import { router as huarongDaoRouter } from './huarongDao';
 router.use('/huarong-dao', huarongDaoRouter);
 
